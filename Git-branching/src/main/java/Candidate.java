d666ddd7287f01edff4f18add54e32bcdd50f28b
Katarzyna Grocholska <katarzyna.grocholska@ppuc.pl>
2022-03-25 12:12:14 +0100

    Encja Kandydat

---
+++ Git-branching/src/main/java/Candidate.java
@@ -0,0 +1,12 @@
+import java.util.UUID;
+
+public class Candidate extends User {
+    private UUID id;
+    private String location;
+    private String linkCV;
+    private boolean isStudent;
+    private String linkGIT;
+    private String LinkLinkedIn;
+    private boolean consentForFutureRectutations;
+
+}
